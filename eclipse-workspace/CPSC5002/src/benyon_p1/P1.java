853c02e37bb12e202c2970455e974bdf881d13a6
Nick <nicholasbenyo@gmail.com>
2017-07-28 00:58:38 -0700

    Added some new stuff

--- eclipse-workspace/CPSC5002/src/benyon_p1/P1.java
+++ eclipse-workspace/CPSC5002/src/benyon_p1/P1.java
@@ -1,161 +1,170 @@
+/*
+ * Nick Benyo
+ * CPSC 5002, Seattle University
+ * This is free and unencumbered software released into the public domain.
+ */
+
 package benyon_p1;
 
 import java.util.Scanner;
 
 /**
- * P1 class used to play a game of Tic-tac-toe. Formatting, loop execution, and 
- * game board formatting all handled in this class. Game-board manipulation
- * and logic for testing rows/cols/diagonals for wins found in TicTacToe class
+ * P1 class used to play a game of Tic-tac-toe. Prints out the board and
+ * switches turns between X and O until a winner is determined. Keeps
+ * track of score and allows user to play multiple games.
  * @author Nick Benyo
  *
  */
 public class P1 {
-	//data members below used for the game stats
-	private static int owins = 0;//Score for O
-	private static int xwins = 0;//Score for X
-	private static int ties = 0;//Score for ties
-	private static Scanner scanner = new Scanner(System.in);
-	
+	// data members below used for the game stats
+	private static int owins = 0;// Score for O
+	private static int xwins = 0;// Score for X
+	private static int ties = 0;// Score for ties
+	private static int currentRound = 0;// Tracks current round for each game
+	private static Scanner scanner = new Scanner(System.in);// For user input
+
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner playmore = new Scanner(System.in);
-		String keepPlaying = "y";
-		//Player must select y to continue with each game end
-		while(keepPlaying.equals("y")) {
+		String keepPlaying = "y";// Whether or not to continue game
+		System.out.print("Welcome to TicTacToe");
+		while (keepPlaying.equals("y")) {
 			gamePlay();
-			System.out.printf("\nGame Stats\nX has %d wins"
-					+ "\nO has %d wins\nThere have been %d ties\n"
-					, xwins, owins, ties);
+			System.out.printf("\nGame Stats\nX has %d wins" + 
+			"\nO has %d wins\nThere have been %d ties\n", xwins,owins, ties);
 			System.out.print("\nDo you want to play again? (y for yes) ");
-			keepPlaying = playmore.nextLine();//Ask player to keep playing
+			keepPlaying = playmore.nextLine();// Ask player to keep playing
 		}
 		System.out.print("Thank you for playing!");
 	}
-	
+
 	/**
-	 * Gameplay used to initiate the game, will continue until a winner
-	 * is determined or a tie occurs. Adds to the win/loss count using
-	 * owins/xwins/ties data members
+	 * Gameplay used to initiate the game, will continue until a winner is
+	 * determined or a tie occurs. Adds to the win/loss 
+	 * count using owins/xwins/ties data members
 	 */
 	public static void gamePlay() {
-		TicTacToe array = new TicTacToe();
-		int size = array.getBoardSize();
-		int numofsquares = size * size;//Counts number of spaces in game
-		String gameWinner = "Tie";
-		int rounds = 0;
-		boolean over = false;
-		while(!over && rounds < numofsquares) {
-			over = turn(array, "X");
-			if(over) {
-				gameWinner = "X";
-				break;
+		currentRound = 0;
+		TicTacToe currentBoard = new TicTacToe();// Board object for game
+		int size = currentBoard.getBoardSize();// Used to calc spaces on board
+		int numofsquares = size * size;// Calculated spaces in game board
+		String gameWinner = "Tie";// Winner of the game, defaults to tie
+		boolean isGameOver = false;// Game ended from a win
+		String currentPlayer = "X";// Current player during round
+		while (!isGameOver && currentRound < numofsquares) {
+			if (currentRound % 2 == 0) {
+				currentPlayer = "X";
+			} else {
+				currentPlayer = "O";
 			}
-			rounds++;
-			//If statement below kills game when all spaces have been filled.
-			//Moves are odd number of moves and must terminate mid-loop if tie
-			if(rounds == numofsquares) {
-				break;
-			}
-			over = turn(array, "O");
-			if(over) {
-				gameWinner = "O";
-				break;
-			}
-			rounds++;
+			isGameOver = gameRound(currentBoard, currentPlayer);
+			currentRound++;
 		}
-		//If statement to track wins below at end of each game
-		if(gameWinner == "O") {
+		if (isGameOver) {
+			gameWinner = currentPlayer;
+		}
+		// If statement to track wins below at end of each game
+		if (gameWinner == "O") {
 			owins += 1;
-		}else if(gameWinner == "X") {
+		} else if (gameWinner == "X") {
 			xwins += 1;
-		}else {
+		} else {
 			ties += 1;
 		}
-		printBoard(array.getBoard());//Ensure the board is printed on win
-		if(gameWinner!="Tie") {
+		printBoard(currentBoard.getBoard());// Print board on win
+		if (gameWinner != "Tie") {
 			System.out.printf("%s wins the game!\n", gameWinner);
-		}else{
+		} else {
 			System.out.print("No one wins, it's a tie!\n");
-			}
+		}
 	}
-	
+
 	/**
-	 * Executes a full turn of the game, printing the board and calling
-	 * the aPlayerMove method to record a move from the player
-	 * @param gameboard a TicTacToe object to be manipulated by the player
-	 * @param a string value representing the player (X or O)
+	 * Executes a full turn of the game, printing the board and calling the
+	 * aPlayerMove method to record a move from the player
+	 * 
+	 * @param gameboard
+	 *            a TicTacToe object to be manipulated by the player
+	 * @param player 
+	 * 			  string value representing the player (X or O)
 	 * @return a boolean indicating whether or not this turn resulted in a win
 	 */
-	public static boolean turn(TicTacToe gameboard, String player) {
+	public static boolean gameRound(TicTacToe gameboard, String player) {
 		System.out.println();
 		printBoard(gameboard.getBoard());
 		System.out.printf("\n%s, it is your turn.\n", player);
-		aPlayerMove(gameboard, player);
+		userMoveEntry(gameboard, player);
 		return gameboard.win(player);
 	}
-	
+
 	/**
-	 * A method specifically manipulating the TicTacToe object from user input with
-	 * the scanner package. Prints out instructions to the terminal when taking in values
-	 * @param gameboard a TicTacToe object to be manipulated by the player
-	 * @param player a string value representing the player (X or O)
+	 * A method to track moves with the TicTacToe object from user 
+	 * input. Prints out instructions to the terminal 
+	 * when taking in values
+	 * @param gameboard
+	 *            a TicTacToe object to be manipulated by the player
+	 * @param player
+	 *            a string value representing the player (X or O)
 	 */
-	public static void aPlayerMove(TicTacToe gameboard, String player) {
-		boolean correctAnswer = false;
-			//correctAnswer returns a boolean if valid, loop terminates
-			//as soon as the player selects a valid move
-			while(!correctAnswer) {
-				System.out.print("Which row? ");
-				int row = scanner.nextInt();//Reads in player row choice
-				//While loop ensures player selects values in range of array
-				while(row >= gameboard.getBoard().length||row<0) {
-					System.out.println("\nThat number isn't on the board! "
-							+ "Please try again ");
-					row = scanner.nextInt();
-				}
-				System.out.print("Which column? ");
-				int column = scanner.nextInt();
-				while(column >= gameboard.getBoard().length||column<0) {
-					System.out.println("\nThat number isn't on the board! "
-							+ "Please try again ");
-					column = scanner.nextInt();
-				}
-				correctAnswer = gameboard.playerMove(row, column, player);
+	public static void userMoveEntry(TicTacToe gameboard, String player) {
+		boolean correctAnswer = false;// User answer was valid
+		// correctAnswer returns a boolean if valid, loop terminates
+		// as soon as the player selects a valid move
+		while (!correctAnswer) {
+			System.out.print("Which row? ");
+			int row = scanner.nextInt();// Reads in player row choice
+			// While loop ensures player selects values in range of array
+			while (row >= gameboard.getBoard().length || row < 0) {
+				System.out.println("\nThat number isn't on the board! " 
+						+ "Please try again ");
+				row = scanner.nextInt();// Row user selected
 			}
+			System.out.print("Which column? ");
+			int column = scanner.nextInt();
+			while (column >= gameboard.getBoard().length || column < 0) {
+				System.out.println("\nThat number isn't on the board! " 
+						+ "Please try again ");
+				column = scanner.nextInt();// Column user selected
+			}
+			correctAnswer = gameboard.playerMove(row, column, player);
+		}
 	}
-	
+
 	/**
-	 * Method for printing a TicTacToe game-board array in a terminal in
-	 * a terminal optimized format
-	 * @param gameboard a square array of string type
+	 * Method for printing a TicTacToe game-board array in a terminal 
+	 * in an optimized format
+	 * @param gameboard
+	 *            a square array of string type
 	 */
 	public static void printBoard(String[][] gameboard) {
-		//Loop to print through the array
+		// Loop to print through the array
 		int lenvar = gameboard[0].length;
-		for(int rowlen = 0; rowlen < lenvar; rowlen++) {
+		for (int rowlen = 0; rowlen < lenvar; rowlen++) {
 			System.out.printf("  %d", rowlen);
 		}
 		System.out.println();
 		for (int row = 0; row < lenvar; row++) {
-			//If statement to start a new line with each new row
-			if(row>0) {
-				System.out.println();}
-			//Loop through each value in the array
+			// If statement to start a new line with each new row
+			if (row > 0) {
+				System.out.println();
+			}
+			// Loop through each value in the array
 			for (int col = 0; col < lenvar; col++) {
-				//Create indent with each row for diagonals total
-				if(col==0 && row >= 1) {
-					String lines = new String(new char[lenvar]).replace("\0", "---");
-					System.out.print(" "+lines+"\n");
+				// Create indent with each row for diagonals total
+				if (col == 0 && row >= 1) {
+					String lines = 
+							new String(new char[lenvar]).replace("\0", "---");
+					System.out.print(" " + lines + "\n");
 				}
-				if(col==0) {
+				if (col == 0) {
 					System.out.print(row);
 				}
 				System.out.printf(" ");
-				System.out.printf("%s|",gameboard[row][col]);
-				//If the end of the row, print out the totaled variable
+				System.out.printf("%s|", gameboard[row][col]);
+				// If the end of the row, print out the totaled variable
 			}
 		}
 		String lines = new String(new char[lenvar]).replace("\0", "---");
-		System.out.print("\n "+lines+"\n");
+		System.out.print("\n " + lines + "\n");
 	}
 }
--- eclipse-workspace/CPSC5002/src/benyon_p1/TicTacToe.java
+++ eclipse-workspace/CPSC5002/src/benyon_p1/TicTacToe.java
@@ -1,160 +1,175 @@
+/*
+ * Nick Benyo
+ * CPSC 5002, Seattle University
+ * This is free and unencumbered software released into the public domain.
+ */
+
 package benyon_p1;
 
-
 /**
- * TicTacToe class generates an object with a string array used 
- * for playing tic-tac-toe. Contains classes for marking moves 
- * in the array, and for testing rows, columns, and diagonals 
- * to determin if a win has occurred. Wins result in a true boolean
+ * TicTacToe class used to create and track a game of Tic Tac Toe. Checks for
+ * winners and has flexible board sizes
+ * 
  * @author Nick Benyo
  *
  */
 public class TicTacToe {
-	private String[][] gameBoard;//An array used to track the values in game
-	private int boardSize = 3;//The size of the gameboard (n X n)
-	
+	private String[][] gameBoard;// An array used to track the values in game
+	private int boardSize = 3;// The size of the game-board (n X n)
+
 	/**
-	 * Initializes TicTacToe object, will build an array 
-	 * used to track the game
-	 * @param size the size of the game-board/array
+	 * Initializes TicTacToe object, will build an array used to track the game
 	 */
 	public TicTacToe() {
-		this.gameBoard = array(boardSize);
+		generateBlankBoard();
 	}
-	
+
 	/**
 	 * Setter to change board size variable and reinitialize board
-	 * @param size The square size of the board
+	 * 
+	 * @param size
+	 *            The square size of the board
 	 */
 	public void setBoardSize(int size) {
 		this.boardSize = size;
-		this.gameBoard = array(boardSize);
+		generateBlankBoard();
 	}
-	
+
 	/**
-	 * Getter for size of board. 
+	 * Getter for size of board.
+	 * 
 	 * @return Integer value of board size (n X n)
 	 */
 	public int getBoardSize() {
 		return this.boardSize;
 	}
-	
+
 	/**
 	 * Array, builds the array used for the game-board data member
-	 * @param size of the array initialized
-	 * @return Returns a size x size array
 	 */
-	public static String[][] array(int size) {
-		int ROWS = size;
-		int COLS = size;
-		String[][] x  = new String[ROWS][COLS];
-		//Nested loop assigning random numbers to each value in the array
-		for (int row = 0; row < ROWS; row++){		
+	public void generateBlankBoard() {
+		int ROWS = this.boardSize;
+		int COLS = this.boardSize;
+		String[][] x = new String[ROWS][COLS];
+		// Nested loop assigning random numbers to each value in the array
+		for (int row = 0; row < ROWS; row++) {
 			for (int col = 0; col < COLS; col++) {
 				String blanks = " ";
-				x[row][col] = blanks;//Assigns blank space array positions
+				x[row][col] = blanks;// Assigns blank space array positions
 			}
 		}
-		return x;//finalized array with blank space
+		this.gameBoard = x;// finalized board with blank values
 	}
-	
+
 	/**
 	 * Used as a getter for the game-board data member
+	 * 
 	 * @return the game-board array data member
 	 */
-	public String[][] getBoard(){
+	public String[][] getBoard() {
 		return this.gameBoard;
 	}
-	
+
 	/**
-	 * Use to execute a move for a player. Will print to terminal if 
-	 * the space has already been taken and return a boolean indicating
-	 * if the move is valid.
-	 * @param row the row of the 2D game-board array to be filled
-	 * @param col the column of the 2D game-board array to be filled
-	 * @param player a string value representing the player (X or O)
+	 * Use to execute a move for a player. Will print to terminal if the 
+	 * space has already been taken and return a boolean indicating if 
+	 * the move is valid.
+	 * 
+	 * @param row
+	 *            the row of the 2D game-board array to be filled
+	 * @param col
+	 *            the column of the 2D game-board array to be filled
+	 * @param player
+	 *            a string value representing the player (X or O)
 	 * @return Returns a boolean indicating if the move was valid
 	 */
 	public boolean playerMove(int row, int col, String player) {
-		boolean valid = true;
+		boolean valid = true;// The passed through move is valid
 		if (this.gameBoard[row][col] == " ") {
 			this.gameBoard[row][col] = player;
-		}else {
+		} else {
 			System.out.print("That space is already taken!\n");
 			valid = false;
 		}
 		return valid;
 	}
-	
+
 	/**
 	 * Use to determine if a winning configuration exists on the game-board
-	 * @param player a string of the player/value on the board (X or O)
-	 * @return a boolean value indicating whether or not the player passed
+	 * 
+	 * @param player
+	 *            a string of the player/value on the board (X or O)
+	 * @return a boolean value indicating whether or not the player passed 
 	 * through is a winner
 	 */
 	public boolean win(String player) {
-		boolean winner = false;
-		if (diagWin(player)||rowWin(player)|| colWin(player) == true) {
+		boolean winner = false;// Whether player won the game
+		if (diagWin(player) || rowWin(player) || colWin(player) == true) {
 			winner = true;
 		}
 		return winner;
 	}
-	
+
 	/**
 	 * Evaluates if a win has occurred on the row level
-	 * @param player a string of the player/value on the board (X or O)
+	 * 
+	 * @param player
+	 *            a string of the player/value on the board (X or O)
 	 * @return a boolean value indicating if the player has won on a row level
 	 */
-	public boolean rowWin(String player) {
-		boolean rowwinner = false;
-		boolean win = true;
+	private boolean rowWin(String player) {
+		boolean rowwinner = false;// A win occurred at some point
+		boolean win = true;// The row tested was a win
 		boolean[] passfail = new boolean[this.gameBoard.length];
 		for (int row = 0; row < this.gameBoard.length; row++) {
 			win = true;
 			for (int col = 0; col < this.gameBoard[row].length; col++) {
-				//Compares every value in row to player value
-				if(player!= this.gameBoard[row][col]) {
-					//win becomes false if a mismatch occurs in row
+				// Compares every value in row to player value
+				if (player != this.gameBoard[row][col]) {
+					// win becomes false if a mismatch occurs in row
 					win = false;
 				}
 			}
-			//adds win boolean to the passfail array
+			// adds win boolean to the passfail array
 			passfail[row] = win;
 		}
-		//for loop iterates through every value of passfail looking for
-		//a true value (true = a win)
-		for(boolean gameover:passfail) {
+		// for loop iterates through every value of passfail looking for
+		// a true value (true = a win)
+		for (boolean gameover : passfail) {
 			if (gameover == true) {
-				//Assigns a true to rowwinner of a true value is found
+				// Assigns a true to rowwinner of a true value is found
 				rowwinner = true;
 				break;
 			}
 		}
-		return rowwinner;//Returns a boolean if there was a row win at any point
+		return rowwinner;// Returns a boolean if there was a row win
 	}
-	
+
 	/**
 	 * Evaluates if a win has occurred on the column level
-	 * @param player a string of the player/value on the board (X or O)
+	 * 
+	 * @param player
+	 *            a string of the player/value on the board (X or O)
 	 * @return a boolean value indicating if the player has won on a row level
 	 */
-	public boolean colWin(String player) {
-		/*Functions nearly the same as rowWin but inverted to test
-		 *  column wins instead of rows
+	private boolean colWin(String player) {
+		/*
+		 * Functions nearly the same as rowWin but inverted to test 
+		 * column wins instead of rows
 		 */
-		boolean colwinner = false;
-		boolean win = true;
+		boolean colwinner = false;// A win occurred at some point
+		boolean win = true;// The column tested was a win
 		boolean[] passfail = new boolean[this.gameBoard.length];
 		for (int col = 0; col < this.gameBoard[0].length; col++) {
 			win = true;
 			for (int row = 0; row < this.gameBoard.length; row++) {
-				if(player!= this.gameBoard[row][col]) {
+				if (player != this.gameBoard[row][col]) {
 					win = false;
 				}
 			}
 			passfail[col] = win;
 		}
-		for(boolean gameover:passfail) {
+		for (boolean gameover : passfail) {
 			if (gameover == true) {
 				colwinner = true;
 				break;
@@ -162,35 +177,37 @@ public class TicTacToe {
 		}
 		return colwinner;
 	}
-	
+
 	/**
 	 * Evaluates if a win has occurred on the diagonal level
-	 * @param player a string of the player/value on the board (X or O)
-	 * @return a boolean value indicating if the 
-	 * player has won on a diagonal level
+	 * 
+	 * @param player
+	 *            a string of the player/value on the board (X or O)
+	 * @return a boolean value indicating if the player has won on a 
+	 * diagonal level
 	 */
-	public boolean diagWin(String player) {
+	private boolean diagWin(String player) {
 		boolean[] passfail = new boolean[2];
-		boolean winner = false;
-		boolean win = true;
-		int rdiag = this.gameBoard.length-1;
-		//Loops to test diagonal from bottom left to top right
+		boolean winner = false;// There was a win at some point
+		boolean win = true;// The diagonal tested was a win
+		int rdiag = this.gameBoard.length - 1;
+		// Loops to test diagonal from bottom left to top right
 		for (int diag = 0; diag < this.gameBoard.length; diag++) {
-			if(player!= this.gameBoard[diag][rdiag]) {
+			if (player != this.gameBoard[diag][rdiag]) {
 				win = false;
 			}
 			rdiag--;
 		}
 		passfail[0] = win;
 		win = true;
-		//Loops to test diagonal from top left to bottom right
+		// Loops to test diagonal from top left to bottom right
 		for (int diag = 0; diag < this.gameBoard.length; diag++) {
-			if(player!= this.gameBoard[diag][diag]) {
+			if (player != this.gameBoard[diag][diag]) {
 				win = false;
 			}
 		}
 		passfail[1] = win;
-		for(boolean gameover:passfail) {
+		for (boolean gameover : passfail) {
 			if (gameover == true) {
 				winner = true;
 				break;
@@ -198,6 +215,5 @@ public class TicTacToe {
 		}
 		return winner;
 	}
-
 
 }
